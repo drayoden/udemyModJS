bcdb5510eb18782d5ad7894131ca6468f3da0dcb
Daryn Oden <daryn@hex50.com>
2020-03-21 19:15:19 -0700

    remove personal access token

--- apiProj/oauth/app.js
+++ apiProj/oauth/app.js
@@ -2,7 +2,7 @@
  
 async function getUser(url) {
     
-    const paToken = 'a115ec2a90bdf682f9571e1db2c066b1862df320';
+    const paToken = '';
     
     const r = await fetch(url, {
         method: 'GET',
@@ -19,6 +19,3 @@ getUser('https://api.github.com/users/dzz3')
     .then(data => console.log(data.p.message));
 
 
-// personal access token:
-// a115ec2a90bdf682f9571e1db2c066b1862df320
-        
