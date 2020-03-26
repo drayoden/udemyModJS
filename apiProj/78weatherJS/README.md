b454d1c16c1ac5fc620182e326f02883caaa205d
daryn <daryn@hex50.com>
2020-03-26 15:37:55 -0700

    sub: updated REAME.md and apps.js
    det:

--- apiProj/78weatherJS/README.md
+++ apiProj/78weatherJS/README.md
@@ -1,7 +1,6 @@
 # Keeping my api keys secret
 
 Starting back in the GithubFinder app I successfully used fetch to open
-a local.js file (.gitigore) and set a constructor attribute in the Github class to the api key. 
+a local.json file (.gitigore) and set the value of the apiKey. 
 
-Not, for some reason, I am unable to repeate this. I am completely baffled and have been going around and around without any resolution. So, I have changed to creating a local.js file (.gitignore) and exporting a variable with the api key. I then import it in the Weather
-class constructor.
+For some reason, I am unable to repeate this. I am completely baffled and have been going around and around without any resolution. So, I have changed to creating a local.js with a simple class that holds a single attribute; the apikey. Go figure.
--- apiProj/78weatherJS/app.js
+++ apiProj/78weatherJS/app.js
@@ -2,8 +2,18 @@
 
 const weather = new Weather("Beaverton","Oregon"); 
 
-weather.getWeather()  // getWeather is an 'async' method thus it returns a promise...
-    .then(r => {console.log(r.main.temp, r.main.feels_like )})
-    // .then(r => {console.log(r.wind.speed, r.wind.deg)})
-    // .then(r => {console.log(r.weather[0].main, r.weather[0].description, r.weather[0].icon )})
-    .catch(e => console.log(e));
+// wrap getWeather in a fucntion to load when the dom loads...
+
+document.addEventListener('DOMContentLoaded', getWeather);
+
+funtion getWeather() {
+    weather.getWeather()  // getWeather is an 'async' method thus it returns a promise...
+        .then(r => {console.log(r.main.temp, r.main.feels_like )})
+        // .then(r => {console.log(r.wind.speed, r.wind.deg)})
+        // .then(r => {console.log(r.weather[0].main, r.weather[0].description, r.weather[0].icon )})
+        .catch(e => console.log(e));
+}
+
+
+
+// convert C to F => (C x 9/5) + 32 
