8c23b9e55b00f78f6dc1db87f0f8f884f87ef34b
daryn <daryn@hex50.com>
2020-03-26 21:40:55 -0700

    update ui.js with weather data

--- apiProj/78weatherJS/ui.js
+++ apiProj/78weatherJS/ui.js
@@ -14,12 +14,34 @@ class UI {
 
     // The weather attributes are specific to the api (openweathermap)...
     paint(weather, city, state) {
+        let tempC = weather.main.temp;
+        let tempF = (tempC * (9/5)) + 32;
+        
+        // location and weather description
         this.location.textContent = `${city}, ${state}`;
-        this.desc.textContent = `${weather.weather[0].description}`;
-        this.string.textContent = `${weather.main.temp}&#8451;`;
+        this.desc.textContent = `${weather.weather[0].description.toUpperCase()}`;
+        
+        // temps
+        this.string.textContent = `${tempC.toFixed(1)} `;
+        this.string.insertAdjacentHTML('beforeend','&#x2103; &nbsp; &nbsp;');
+        this.string.insertAdjacentHTML('beforeend',`${tempF.toFixed(1)} `);
+        this.string.insertAdjacentHTML('beforeend','&#x2109;');
 
-        console.log(weather.main.temp);
-        //this.desc.textContent
+        // icon
+        this.icon.setAttribute("src",`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+
+        // humidity, 
+        this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`; 
+
+        // dewpoint
+        let TdC = tempC - ((100 - weather.main.humidity)/5)
+        let TdF = (TdC * (9/5)) + 32;
+        this.dewpoint.textContent = `DewPoint: ${TdC.toFixed(1)}`;
+        this.dewpoint.insertAdjacentHTML('beforeend','&#x2103; &nbsp; &nbsp;');
+        this.dewpoint.insertAdjacentHTML('beforeend',`${TdF.toFixed(1)}`);
+        this.dewpoint.insertAdjacentHTML('beforeend','&#x2109;');
+
+        
     }
 }
 
